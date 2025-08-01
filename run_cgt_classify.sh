304c650d191bc504a28cd076028a5cec14ca8940
Stephan Klein <sklein.at@gmail.com>
2025-08-01 15:06:16 +0000

    update dataset

--- run_cgt_classify.sh
+++ run_cgt_classify.sh
@@ -1,8 +1,17 @@
+# python graph_classifier.py \
+#     -ld ./logs/graph_classification/cfg_cg/node_features \
+#     --output_models ./models/graph_classification/cfg_cg/node_features \
+#     --dataset ./experiments/ge-sc-data/source_code/reentrancy/cgt/train \
+#     # --testset ./experiments/ge-sc-data/source_code/reentrancy/cgt/test \
+#     --compressed_graph ./experiments/ge-sc-data/source_code/reentrancy/cgt/cfg_cg_compressed_graphs.gpickle \
+#     --label ./experiments/ge-sc-data/source_code/reentrancy/cgt/mando_labels.json \
+#     --node_feature nodetype \
+#     --seed 1
+
 python graph_classifier.py \
     -ld ./logs/graph_classification/cfg_cg/node_features \
     --output_models ./models/graph_classification/cfg_cg/node_features \
-    --dataset ./experiments/ge-sc-data/source_code/reentrancy/cgt/train \
-    # --testset ./experiments/ge-sc-data/source_code/reentrancy/cgt/test \
+    --dataset ./experiments/ge-sc-data/source_code/reentrancy/cgt \
     --compressed_graph ./experiments/ge-sc-data/source_code/reentrancy/cgt/cfg_cg_compressed_graphs.gpickle \
     --label ./experiments/ge-sc-data/source_code/reentrancy/cgt/mando_labels.json \
     --node_feature nodetype \
